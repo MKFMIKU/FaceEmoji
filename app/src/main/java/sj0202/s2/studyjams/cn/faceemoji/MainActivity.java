680793bce3188bc0de82ae084ff174d52c405b69
MKFMIKU <mikumkf@outlook.com>
2017-05-28 18:01:27 +0800

    Add camera rotate

--- app/src/main/java/sj0202/s2/studyjams/cn/faceemoji/MainActivity.java
+++ app/src/main/java/sj0202/s2/studyjams/cn/faceemoji/MainActivity.java
@@ -3,32 +3,37 @@ package sj0202.s2.studyjams.cn.faceemoji;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
+public class MainActivity extends AppCompatActivity{
     private static final int MY_PERMISSIONS_REQUEST_CAMERA = 0;
+    private int CAMERA_ID = CameraBridgeViewBase.CAMERA_ID_BACK;
     private Mat mRgba;
-    private Mat mIntermediateMat;
-    private Mat mGray;
     private CameraBridgeViewBase cameraBridgeViewBase;
     private static final String TAG = "FaceCV::Main";
 
@@ -37,7 +42,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         public void onManagerConnected(int status) {
             switch (status){
                 case LoaderCallbackInterface.SUCCESS:{
-                    Log.d(TAG, "OpenCV loaded");
                     System.loadLibrary("native-lib");
                     cameraBridgeViewBase.enableView();
                 } break;
@@ -53,8 +57,8 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
                 !=PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},
                     MY_PERMISSIONS_REQUEST_CAMERA);
-        }else {
-            cameraBridgeViewBase.setCvCameraViewListener(this);
+        }else{
+            Toast.makeText(this,"Permission OK!",Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -63,12 +67,41 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
+        init();
 
         cameraBridgeViewBase = (CameraBridgeViewBase) findViewById(R.id.cameraView);
         cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
-        cameraBridgeViewBase.setCameraIndex(CameraBridgeViewBase.CAMERA_ID_BACK);
-        cameraBridgeViewBase.setCvCameraViewListener(this);
-        init();
+        cameraBridgeViewBase.setCameraIndex(CAMERA_ID);
+        cameraBridgeViewBase.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener() {
+            @Override
+            public void onCameraViewStarted(int width, int height) {
+                mRgba = new Mat(width, height, CvType.CV_8UC4);
+            }
+
+            @Override
+            public void onCameraViewStopped() {
+                mRgba.release();
+            }
+
+            @Override
+            public Mat onCameraFrame(Mat inputFrame) {
+                return inputFrame;
+            }
+
+        });
+
+        ImageButton rotateButton = (ImageButton) findViewById(R.id.rotate_button);
+        rotateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraBridgeViewBase.disableView();
+                if(CAMERA_ID==CameraBridgeViewBase.CAMERA_ID_BACK)
+                    CAMERA_ID = CameraBridgeViewBase.CAMERA_ID_FRONT;
+                else CAMERA_ID=CameraBridgeViewBase.CAMERA_ID_BACK;
+                cameraBridgeViewBase.setCameraIndex(CAMERA_ID);
+                cameraBridgeViewBase.enableView();
+            }
+        });
     }
 
     @Override
@@ -96,24 +129,5 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
             Log.d(TAG, "OpenCV loaded");
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }
-    }
-
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-        mRgba = new Mat(width, height, CvType.CV_8UC4);
-        mIntermediateMat = new Mat(height, width, CvType.CV_8UC4);
-        mGray = new Mat(height, width, CvType.CV_8UC1);
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-        mRgba.release();
-        mGray.release();
-        mIntermediateMat.release();
-    }
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        return mRgba;
     }
 }
